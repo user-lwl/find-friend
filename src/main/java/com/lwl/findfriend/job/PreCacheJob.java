9022e00824628663d82c1b46161282e0a5c13cd0
lwl <1399097219@qq.com>
2022-11-11 20:49:03 +0800

    update

--- src/main/java/com/lwl/findfriend/job/PreCacheJob.java
+++ src/main/java/com/lwl/findfriend/job/PreCacheJob.java
@@ -5,6 +5,8 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.lwl.findfriend.model.domain.User;
 import com.lwl.findfriend.service.UserService;
 import lombok.extern.slf4j.Slf4j;
+import org.redisson.api.RLock;
+import org.redisson.api.RedissonClient;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 import org.springframework.scheduling.annotation.Scheduled;
@@ -29,6 +31,9 @@ public class PreCacheJob {
     @Resource
     private RedisTemplate<String, Object> redisTemplate;
 
+    @Resource
+    private RedissonClient redissonClient;
+
     private List<Long> mainUserList = Arrays.asList(1L);
 
     /**
@@ -37,14 +42,21 @@ public class PreCacheJob {
      */
     @Scheduled(cron = "0 58 23 * * *")
     public void doCacheRecommendUser() {
-        for (Long userId : mainUserList) {
-            //查库
-            QueryWrapper<User> queryWrapper = new QueryWrapper<>();
-            Page<User> userPage = userService.page(new Page<>(1, 20), queryWrapper);
-            String redisKey = String.format("findfriend:user:recommend:%s", userId);
-            ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
-            //写缓存
-            valueOperations.set(redisKey, userPage, 60000, TimeUnit.MILLISECONDS);
+        RLock lock = redissonClient.getLock("findfriend:precachejob:docache:lock");
+        try {
+            if (lock.tryLock(0, 30000, TimeUnit.MILLISECONDS)) {
+                for (Long userId : mainUserList) {
+                    //查库
+                    QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+                    Page<User> userPage = userService.page(new Page<>(1, 20), queryWrapper);
+                    String redisKey = String.format("findfriend:user:recommend:%s", userId);
+                    ValueOperations<String, Object> valueOperations = redisTemplate.opsForValue();
+                    //写缓存
+                    valueOperations.set(redisKey, userPage, 60000, TimeUnit.MILLISECONDS);
+                }
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
     }
 }
