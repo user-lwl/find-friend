33b607b50a2c0115060ac2490c157399767598b4
lwl <1399097219@qq.com>
2022-11-11 15:05:30 +0800

    idea

---
+++ src/main/java/com/lwl/findfriend/config/RedisTemplateConfig.java
@@ -0,0 +1,18 @@
+package com.lwl.findfriend.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.core.RedisTemplate;
+
+/**
+ * @author user-lwl
+ * @date 2022/11/11 14:59
+ */
+@Configuration
+public class RedisTemplateConfig {
+
+//    @Bean
+//    public RedisTemplate redisTemplate() {
+//
+//    }
+}
